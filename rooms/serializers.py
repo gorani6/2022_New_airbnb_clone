0bcab9470feb2f348a6ae72af06e2744dfe180c8
yeardeam <gorani_6@naver.com>
2022-11-16 17:21:20 +0900

    2022.11.16

---
+++ rooms/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from .models import Amenity, Room
+from users.serializers import TinyUserSrializer
+from categories.serializers import CategorySerializer
+from rest_framework import serializers
+from reviews.serializers import ReviewSerializer
+
+class AmenitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Amenity
+        fields = ("name", "description")
+
+class RoomDetailSerializer(serializers.ModelSerializer):
+
+    owner = TinyUserSrializer(read_only=True)
+    amenities = AmenitySerializer(read_only=True, many=True)
+    catogory = CategorySerializer(read_only=True)
+    rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
+    
+
+    class Meta:
+        model = Room
+        fields = "__all__"
+
+    def get_rating(self, room):
+        return room.rating()
+    
+    def get_is_owner(self, room):
+        request = self.context['request']
+        return room.owner == request.user
+
+
+
+class RoomListSerializer(serializers.ModelSerializer):
+
+    rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Room
+        fields = (
+            "pk",
+            "name",
+            "country",
+            "city",
+            "price",
+            "rating",
+            "is_owner",
+        )
+    def get_rating(self, room):
+        return room.rating()
+    def get_is_owner(self, room):
+        request = self.context['request']
+        return room.owner == request.user
+
+

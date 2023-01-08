06795b38efa8b48d8ed628872bd670ce97a0439a
yeardeam <gorani_6@naver.com>
2023-01-09 08:02:37 +0900

    2023.1.9

--- config/settings.py
+++ config/settings.py
@@ -11,17 +11,23 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-im)glcvcmr^j7-6l8x75owyheit+g%bs3fzw-%1f&&9f=xf)d='
-
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -31,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 TRIRD_PARTY_APPS = [
     "rest_framework",
+    "rest_framework.authtoken",
 ]
 
 
@@ -149,5 +156,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'config.authentication.TrustMeBroAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        "config.authentication.JWTAuthentication",
     ]
 }
